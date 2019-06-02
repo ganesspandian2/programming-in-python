f40faf844d3e7db2e7eba39a82cd5236447911d6
NAVIN GANESH PANDIYAN R K <ganesspandian2@gmail.com>
2019-06-02 14:55:11 +0530

    PROGRAM TO CHECK WHELTER THE GIVEN YEAR IS LEAP or NOT

---
+++ leapornot.py
@@ -0,0 +1,6 @@
+#PROGRAM TO FIND WHELTER THE GIVEN YEAR IS A LEAP YEAR or NOT
+n=int(input("ENTER THE YEAR\n"))
+if n%4==0:
+    print("The given year is a Leap Year")    
+else:
+    print("The given year is not a Leap Year")    
