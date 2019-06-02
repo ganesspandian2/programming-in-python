909b440131fa2414d13f1d93fccaddf31cd592e8
NAVIN GANESH PANDIYAN R K <ganesspandian2@gmail.com>
2019-06-02 14:50:52 +0530

    PROGRAM TO CHECK WHELTER THE NUMBER IS ODD or EVEN

---
+++ evenorodd.py
@@ -0,0 +1,7 @@
+#PROGRAM TO CHECK WHELTER THE NUMBER IS EVEN or ODD
+n=int(input())
+if n%2==0:
+    print("The given number is an Even Number")
+else:
+    print("The given number is an Odd Number")
+    
