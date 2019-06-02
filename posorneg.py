f398a5c0e9225549bd858d64766a9c24ca990f1e
NAVIN GANESH PANDIYAN R K <ganesspandian2@gmail.com>
2019-06-02 15:03:23 +0530

    PROGRAM TO CHECK WHELTER THE GIVEN NUMBER IS A POSITIVE or NEGATIVE NUMBER

---
+++ posorneg.py
@@ -0,0 +1,6 @@
+#PROGRAM TO FIND THE NUMBER WHELTER IT IS A POSITIVE or NEGATIVE NUMBER
+n=int(input())
+if n<0:
+    print("The given number is a negative number")
+else:
+    print("The given number is a positive number")
